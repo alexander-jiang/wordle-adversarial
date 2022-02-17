928ea4286623287687c666688bee191fc9f1f5e1
Alexander Jiang <dashuai99@gmail.com>
2022-02-16 23:14:48 -0800

    Update helper CLI tool with 'helpful' guesses: first, ignore guesses that use the gray letters

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## TODO
 - implement a wordlist searcher that builds a trie and then brute forces all possible resolutions of yellow letters -> green letters. e.g. if first guess is "bread" with clues RRYGR, then the word must be either E__A_, _E_A_, or ___AE (the trie will visit all children if the letter is not specified)
+- more clever guess suggestions: returns valid guesses that do not use any of the grayed out letters
+    - (additional feature: only include valid guesses which use yellow letters, but not in the marked-yellow positions, or in positions that already have a green letter)
 
 ## Dev Env setup
 ```bash
--- wordle/cli/helper.py
+++ wordle/cli/helper.py
@@ -2,10 +2,6 @@
 CLI tool to run a Wordle "helper" program: you input the clue state
 (i.e. word you tried, and which letters were colored gray, yellow, or green)
 and it returns a list of possible answer words from the list
-
-todo: maybe also returns valid guesses that do not use any of the grayed out letters
-(additional feature: only include valid guesses which use yellow letters, but not in the 
-marked-yellow positions, or in positions that already have a green letter)
 """
 import click
 from typing import List
@@ -69,63 +65,71 @@ def main(guess_wordlist_path, answer_wordlist_path):
     num_answer_words = answer_list_searcher.load_words(answer_wordlist)
     click.echo(f"Done! loaded {num_answer_words} answer words")
 
-    total_clues = WordleClues()
-    next_guess = True
-    total_guess_wordlist = set(guess_wordlist + answer_wordlist)
+    actual_clues = WordleClues()
+    helpful_guess_clues = WordleClues()
+    total_guess_wordlist = set(guess_wordlist)
     possible_answers = set(answer_wordlist)
-    while next_guess:
-        next_guess = False
-
-        guess_prompt = "Please enter your guess\n"
+    gray_letters = set()
+    yellow_letters = set()
+    green_letters = set()
+    while True:
+        guess_prompt = "Your guess\n"
         guess = click.prompt(guess_prompt, type=str)
         while not _is_valid_guess(guess) or guess not in total_guess_wordlist:
             if not _is_valid_guess(guess):
-                click.echo('Your guess was not correctly formatted, please try again')
+                click.echo("Your guess was not valid, please try again")
             if guess not in total_guess_wordlist:
-                click.echo('Your guess was not in the word list, please try again')
+                click.echo("Your guess was not in the word list, please try again")
             guess = click.prompt(guess_prompt, type=str)
         guess = guess.lower()
 
         clues_prompt = (
-            "Please enter the colors/clues (use R for gRay, Y for Yellow, G for Green)\n"
+            "The revealed colors (use R for gRay, Y for Yellow, G for Green)\n"
         )
         clues = click.prompt(clues_prompt, type=str)
         while not _is_valid_clues(clues):
-            click.echo('Your clues string was not correctly formatted, please try again')
+            click.echo("Your colors were not valid, please try again")
             clues = click.prompt(clues_prompt, type=str)
         clues = clues.lower()
 
         for idx in range(WORDLE_COLUMNS):
             if clues[idx] == "r":
                 color = WordleLetterState.GRAY
+                gray_letters.add(guess[idx])
             elif clues[idx] == "y":
                 color = WordleLetterState.YELLOW
+                yellow_letters.add(guess[idx])
             elif clues[idx] == "g":
                 color = WordleLetterState.GREEN
+                green_letters.add(guess[idx])
             else:
                 raise Exception("should not get here: user input for clues is invalid")
-            total_clues.mark(guess[idx], color, idx)
+            actual_clues.mark(guess[idx], color, idx)
 
-        matching_guess_words = guess_list_searcher.match(total_clues)
-        print(f"Found {len(matching_guess_words)} matching guess words!")
-        guesses_sample = matching_guess_words[:10]
-        print(f"some guesses that align with the clues so far: {guesses_sample}")
+            if color == WordleLetterState.GRAY:
+                helpful_guess_clues.mark(guess[idx], color, idx)
 
-        matching_answer_words = answer_list_searcher.match(total_clues)
-        print(f"Found {len(matching_answer_words)} matching answer words!")
-        answers_sample = matching_answer_words[:10]
-        print(f"some potential answers: {answers_sample}")
+        helpful_guesses = guess_list_searcher.match(helpful_guess_clues)
+        print(
+            f"{len(helpful_guesses)} potential guesses that don't use the gray letters ({gray_letters}):"
+        )
+        print(", ".join(sorted(helpful_guesses)))
+
+        matching_answer_words = answer_list_searcher.match(actual_clues)
+        print(f"Found {len(matching_answer_words)} matching answer words:")
+        print(", ".join(sorted(matching_answer_words)))
         possible_answers = set(matching_answer_words)
 
         if len(possible_answers) == 1:
-            click.echo(f'There is only one possible answer word left: {list(possible_answers)[0]}')
+            click.echo(
+                f"There is only one possible answer word left: {list(possible_answers)[0]}"
+            )
             break
         elif len(possible_answers) == 0:
-            click.echo(f'There are no possible answer words left, something went wrong...')
+            click.echo(
+                f"There are no possible answer words left, something went wrong..."
+            )
             break
-
-        next_guess = click.confirm("Do you want to continue?")
-        
 
     click.echo("Goodbye!")
 
